bc612decdd7218b96c33c854fe9fd962213f8158
Jeetul <jeetulsamaiya.js@gmail.com>
2022-11-27 05:27:16 +0530

    3:57 product rating

--- BACKEND/Routes/userRoute.js
+++ BACKEND/Routes/userRoute.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const {
-    registerUser, loginUser,logout, forgotPassword
+    registerUser, 
+    loginUser,
+    logout,
+    forgotPassword,
+    resetPassword,
+    getUserDetail,
+    updatePassword,
+    updateProfile,
+    allUsers,
+    getUser,
+    updateUser,
+    deleteUser
 } = require('../controllers/UserController')
+const { isAuthenticatedUser ,authorizeRoles} = require('../middleware/auth')
+
+
 
 
 // Register a user => /api/v1/register
@@ -16,6 +30,30 @@ router.route('/logout').get(logout)
 
 // Forgot password => /api/v1/password/forgot
 router.route('/password/forgot').post(forgotPassword)
+
+// Reset password => /api/v1/password/reset/:token
+router.route('/password/reset/:token').put(resetPassword)
+
+// Get user profile => /api/v1/me
+router.route('/me').get(isAuthenticatedUser, getUserDetail)
+
+// Update password => /api/v1/password/update
+router.route('/password/update').put(isAuthenticatedUser, updatePassword)
+
+// Update user profile => /api/v1/me/update
+router.route('/me/update').put(isAuthenticatedUser, updateProfile)
+
+// Get all users => /api/v1/admin/users
+router.route('/admin/users').get(isAuthenticatedUser,authorizeRoles('admin'), allUsers)
+
+// Get user details => /api/v1/admin/user/:id
+router.route('/admin/user/:id').get(isAuthenticatedUser,authorizeRoles('admin'), getUser)
+
+// Update user => /api/v1/admin/user/:id
+router.route('/admin/user/:id').put(isAuthenticatedUser,authorizeRoles('admin'), updateUser)
+
+// Delete user => /api/v1/admin/user/:id
+router.route('/admin/user/:id').delete(isAuthenticatedUser,authorizeRoles('admin'), deleteUser)
 
 
 module.exports = router
