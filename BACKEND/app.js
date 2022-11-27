2a2be9332753ea36815b5452a56f47200d9d1fd5
Jeetul <jeetulsamaiya.js@gmail.com>
2022-11-27 18:47:19 +0530

    Backend done Expect payment and cloundnary

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -8,10 +8,12 @@ app.use(cookieParser());
 // route imports
 const productRoute = require('./Routes/productRoute');
 const userRoute = require('./Routes/userRoute');
+const orderRoute = require('./Routes/orderRoute');
 
 // middleware imports
 app.use('/api/v1', productRoute);
 app.use('/api/v1', userRoute);
+app.use('/api/v1', orderRoute);
 
 
 
