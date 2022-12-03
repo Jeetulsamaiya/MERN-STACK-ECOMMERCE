ab3a23f752d05b18c9e7dfc76b345fb09dfa0ed1
Jeetul <jeetulsamaiya.js@gmail.com>
2022-12-03 17:58:13 +0530

    react-navbar-02

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,51 +1,53 @@
 import React from 'react'
 import { ReactNavbar } from 'overlay-navbar'
 import logo from '../../images/logo.png'
+import { MdAccountCircle , MdSearch , MdAddShoppingCart } from 'react-icons/md'
 
+
+const options = {
+    burgerColor: '#464646',
+    burgerColorHover: "black",
+    logo,
+    logoWidth: "20vmax",
+    navColor1: "#7e7e7e",
+    navColor4:"#7e7e7e",
+    logoHoverSize: "10px",
+    logoHoverColor: "black",
+    link1Decoration: "none",
+    link1Text: "Home",
+    link2Text: "Products",
+    link3Text: "Contact",
+    link4Text: "About",
+    link1Url: "/",
+    link2Url: "/products",
+    link3Url: "/contact",
+    link4Url: "/about",
+    link1Size: "1.3vmax",
+    link1Color: "rgba(35, 35, 35,0.8)",
+    nav1justifyContent: "flex-end",
+    nav2justifyContent: "flex-end",
+    nav3justifyContent: "flex-start",
+    nav4justifyContent: "flex-start",
+    link1ColorHover: "black",
+    link1Margin: "1vmax",
+    searchIconColor: "rgba(35, 35, 35,0.8)",
+    cartIconColor: "rgba(35, 35, 35,0.8)",
+    profileIconColor: "rgba(35, 35, 35,0.8)",
+    profileIconColorHover: "black",
+    CartIconColorHover: "black",
+    searchIconColorHover: "black",
+    cartIconMargin: "1vmax",
+    profileIcon: true,
+    ProfileIconElement: MdAccountCircle,
+    searchIcon: true,
+    SearchIconElement: MdSearch,
+    cartIcon: true,
+    CartIconElement: MdAddShoppingCart,
+  };
 
 const Header = () => {
   return(
-    <ReactNavbar 
-    logo={logo}
-    navColor1="white"
-    burgerColor="grey"
-    burgerColorHover="black"
-    logoWidth="15vmax"
-    logoHoverSize="20px"
-    logoHoverColor="grey"
-    link1Text="Home"
-    link2Text="Product"
-    link3Text="Contact"
-    link4Text="About"
-    link1Url = "/"
-    link2Url = "/product"
-    link3Url = "/contact"
-    link4Url = "/about"
-    link1Size="1.3vmax"
-    link2Size="1.3vmax"
-    link3Size="1.3vmax"
-    link4Size="1.3vmax"
-    link1Color="#007CC3"
-    link2Color="#007CC3"
-    link3Color="#007CC3"
-    link4Color="#007CC3"
-    link2ColorHover="#1D4670"
-    link3ColorHover="#1D4670"
-    link4ColorHover="#1D4670"
-    link1ColorHover="#1D4670"
-    nav1justifyContent="flex-end"
-    nav2justifyContent="flex-end"
-    nav3justifyContent="flex-start"
-    nav4justifyContent="flex-start"
-    link1Margin="0vmax"
-    link2Margin="1.5vmax"
-    link3Margin="0vmax"
-    link4Margin="1.5vmax"
-   
-
-
-
-     />
+    <ReactNavbar {...options} />
       ) 
 }
 
